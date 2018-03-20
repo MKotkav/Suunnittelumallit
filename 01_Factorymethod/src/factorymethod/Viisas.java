1d8d1009d65f7639e363786dafab491642611948
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2018-03-20 13:14:31 +0200

    Added project folder for task 01. Added classes Viini, Viisas, Kahvi, Opiskelija. Task completed.

---
+++ 01_Factorymethod/src/factorymethod/Viisas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorymethod;
+
+/**
+ *
+ * @author mikae
+ */
+public class Viisas extends AterioivaOtus{
+
+    @Override
+    public Juoma createJuoma() {
+        return new Viini();
+    }
+    
+}
